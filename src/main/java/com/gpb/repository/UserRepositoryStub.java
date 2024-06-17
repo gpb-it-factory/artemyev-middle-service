bfbd3deb32b0e3b87bbaecadafd69a63b5c46a5d
Nickolay Artemyev <Artemyev1000@gmail.com>
2024-06-17 22:17:19 +0300

    Задание 7. Исправлены замечания - 3.

--- src/main/java/com/gpb/repository/UserRepositoryStub.java
+++ src/main/java/com/gpb/repository/UserRepositoryStub.java
@@ -4,19 +4,20 @@ import com.gpb.entity.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Repository
 public class UserRepositoryStub implements UserRepository {
-    private final ConcurrentHashMap<Long, String> users = new ConcurrentHashMap<>();
-
+    private final Set<User> users = new CopyOnWriteArraySet<>();
     @Override
     public Optional<User> findById(long id) {
-        return users.containsKey(id) ? Optional.of(new User((id), users.get(id))) : Optional.empty();
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst();
     }
 
     @Override
     public void save(User user) {
-        users.put(user.getId(), user.getName());
+        users.add(user);
     }
 }
